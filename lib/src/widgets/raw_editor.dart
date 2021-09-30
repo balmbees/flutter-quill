fca281850c0f6a51585bfee883d5a6605dbd2809
yeonsang an <ys.ahn@canfoundation.io>
2021-09-30 11:00:26 +0900

    fix for flutter 2.5

--- lib/src/widgets/raw_editor.dart
+++ lib/src/widgets/raw_editor.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart' hide KeyboardListener;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide KeyboardListener;
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
